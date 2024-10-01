068bef3c6c67c8883662d6e0005c6ac1306532c8
xuzhuozhen <xuzhuozhen0709@163.com>
2024-10-01 12:24:06 +0800

    luogu Array No.1

--- Array/1/1.cpp
+++ Array/1/1.cpp
@@ -1,3 +1,35 @@
 //
 // Created by 16679 on 2024/10/1.
 //
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> solve(vector<int> &nums) {
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); ++i) {
+        int temp = 0;
+        for (int j = 0; j < i; ++j) {
+            if (nums[i] > nums[j]) temp++;
+        }
+        ans.push_back(temp);
+    }
+
+    return ans;
+}
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) cin >> nums[i];
+
+    auto ans = solve(nums);
+    for (const auto& num: ans) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
